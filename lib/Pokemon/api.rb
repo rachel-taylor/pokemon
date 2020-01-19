e9ad3ff443bb3871f47a2946d54baafbbe8afe3d
Rachel T <rachmaybe@gmail.com>
2020-01-19 06:54:00 +0000

    Refactored Code

--- lib/Pokemon/api.rb
+++ lib/Pokemon/api.rb
@@ -1,8 +1,18 @@
 class Pokemon::API
   
-  def get_name
-   name = open("https://pokeapi.co/api/v2/").read
-   json = JSON.parse(response)
+  def self.get_name(num)
+   poke = open("https://pokeapi.co/api/v2/pokemon/#{num}/").read
+   json = JSON.parse(poke)
+   return json["name"]
+  # binding.pry
   end 
   
+  def self.get_abilities(num)
+    poke = open("https://pokeapi.co/api/v2/pokemon/#{num}/").read
+    json = JSON.parse(poke)
+    return json["abilities"].map do |ability|
+      ability["ability"]["name"]
+      # binding.pry
+    end 
+  end 
 end 
--- lib/Pokemon/cli.rb
+++ lib/Pokemon/cli.rb
@@ -5,16 +5,27 @@ class Pokemon::CLI
     end 
     
     def greeting 
-      puts "Welcome to the Unova"
+      puts "Welcome to Unova"
       puts "Please input a number between 1-100" #actually 897 Pokemon in Pokedex 
-      gets.chomp.to_i
+      # gets.chomp.to_i
+      @num = gets.chomp.to_i
+      puts Pokemon::API.get_name(@num)
+      pokemon 
     end 
     
     def pokemon 
-      #puts name of pokemon
-      puts "Would you like more information? Y/N?"
+      puts "Would you like more information? y/n?"
+      if gets.chomp == 'y'
+        evolution 
+      else 
+        goodbye 
+      end 
       #if yes put pokemon_info 
       #if no call goodbye
+    end 
+    
+    def evolution
+      puts Pokemon::API.get_abilities(@num)
     end 
     
     def goodbye
