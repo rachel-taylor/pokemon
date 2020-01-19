b0956476b491b93090b1187d69502abaabf015f8
Rachel T <rachmaybe@gmail.com>
2020-01-19 07:59:46 +0000

    Cleaned required files

--- lib/Pokemon.rb
+++ lib/Pokemon.rb
@@ -1,8 +1,6 @@
 require 'pry'
 require 'json'
 require 'httparty'
-require 'rest-client'
-require 'exits'
 require 'open-uri'
 
 require_relative "./Pokemon/version"
@@ -10,6 +8,6 @@ require_relative "./Pokemon/api"
 require_relative "./Pokemon/cli"
 
 module Pokemon
-  class Error < StandardError; end
-  # Your code goes here...
+  class Error < StandardError; 
+    end
 end
